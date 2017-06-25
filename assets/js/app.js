a3bfa8abd0920748c44f79ba2db7b1e23c99bde5
Jose DeLavalle <me@slickwebstudio.com>
2017-06-25 19:51:05 -0400

    user location/weather, contact form

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,46 @@ var app = angular.module('app', [
         'material.svgAssetsCache'
     
 ]);
+app.factory('pdFactory', ['$http', '$window', '$q', function ($http, $window, $q) {
+    return {
+        getUserLocation: function() {
+            var deferred = $q.defer();
+
+            if (!$window.navigator.geolocation) {
+                deferred.reject('Geolocation not supported.');
+            } else {
+                $window.navigator.geolocation.getCurrentPosition(
+                    function (position) {
+                        deferred.resolve(position);
+                    },
+                    function (err) {
+                        deferred.reject(err);
+                    });
+            }
+
+            return deferred.promise;
+        },
+        getUserLocationBackup: function() {
+            return $http.get('http://ipinfo.io/json');
+        },
+        reverseGecode: function(loc) {
+        	var thisUrl = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + loc + '&components=administrative_area&key=AIzaSyAFzBg6EWivP2e2GR0DmXdosJKqJylV9AQ';
+        	console.log('reversegeocodeurl', thisUrl);
+            return $http.get(thisUrl);
+        },
+        getWeather: function(lat, lon, country) {
+			var thisUrl = "http://api.openweathermap.org/data/2.5/weather";
+            var uriParameters = "?lat=" + lat + "&lon=" + lon + "&appid=3e2c248c8a21fbe1997eb778a2f2c36c";
+            if (country == "US") {
+                uriParameters += "&units=imperial";
+            } else {
+                uriParameters += "&units=metric";
+            }
+        	return $http.get(thisUrl + uriParameters);
+        }
+        
+    };
+}]);
 
 app.directive('onDragEnd', function() {
     return {
@@ -18,25 +58,93 @@ app.directive('onDragEnd', function() {
 });
 app.controller('portfolioDashboardController', portfolioDashboardController);
 
-portfolioDashboardController.$inject = ['$scope', '$interval', '$timeout', '$mdMedia', '$mdColorPalette', 'NgMap'];
+portfolioDashboardController.$inject = ['$scope', '$interval', '$timeout', 'pdFactory', '$mdMedia', '$mdColorPalette', 'NgMap'];
 
-function portfolioDashboardController($scope, $interval, $timeout, $mdMedia, $mdColorPalette, NgMap) {
+function portfolioDashboardController($scope, $interval, $timeout, pdFactory, $mdMedia, $mdColorPalette, NgMap) {
+	console.log('portfolio dashboard controller', Date());
 	var self = this, j= 0, counter = 0;
 	$scope.colorSet = 1;
 	$scope.maxColorSets = 5;
 	$scope.dataSet = 1;
-	console.log('portfolio dashboard controller', Date());
-
-
-	$scope.colors = Object.keys($mdColorPalette);
-	console.log('colors', $scope.colors);
-
-
+	$scope.userLocation = {};
+	$scope.userCoords = null;
+	$scope.showmain = true;
+	// $scope.colors = Object.keys($mdColorPalette);
+	// console.log('colors', $scope.colors);
 	$scope.clock = ""; // initialise the time variable
     $scope.tickInterval = 1000 //ms
 
+	pdFactory.getUserLocation().then(function(res) {
+		console.log('got user location', res);
+		var usercoords = res.coords.latitude + "," + res.coords.longitude;
+		$scope.userCoords = usercoords;
+		doReverseGeocode(usercoords);
+	}).catch(function(e) {
+		console.log('error getting user location', e);
+		pdFactory.getUserLocationBackup().then(function(res) {
+			console.log('got user location backup', res);
+			var usercoords = res.data.loc;
+			$scope.userCoords = usercoords;
+			doReverseGeocode(usercoords);
+		}).catch(function(e) {
+			console.log('error getting user location backup', e);
+		});
+	});
 
-    var tick = function () {
+	function doReverseGeocode(data) {
+		pdFactory.reverseGecode(data).then(function(res) {
+			console.log('reversegeocode', res);
+			parseGeocode(res);
+			var loc = $scope.userCoords.split(',');
+			getWeather(loc[0], loc[1], $scope.userLocation.country);
+		}).catch(function(e) {
+			console.log('error reverse geocoding', e);
+		});
+	}
+
+	function getWeather(lat, lon, country) {
+		pdFactory.getWeather(lat,lon,country).then(function(res) {
+			console.log('got weather', res);
+			$scope.userLocation.temperature = Math.trunc(res.data.main.temp);
+            $scope.userLocation.icon = res.data.weather[0].icon;
+            console.log('---userlocation---', $scope.userLocation);
+		}).catch(function(e) {
+			console.log('error getting weather', e);
+		});
+	}
+
+	function parseGeocode(res) {
+		for (var i = 0; i < res.data.results[0].address_components.length; i++) {
+            var component = res.data.results[0].address_components[i];
+            if (component.types[0] == "locality") {
+                usercity = component.long_name;
+            }
+            if (component.types[0] == "administrative_area_level_1") {
+                userstate = component.long_name;
+            }
+            if (component.types[0] == "country") {
+                $scope.userLocation.country = component.short_name;
+                if ($scope.userLocation.country == 'US') {
+                    $scope.userLocation.weatherUnit = "F";
+                } else {
+                    $scope.userLocation.weatherUnit = "C";
+                }
+            }
+        }
+        if (usercity) {
+            $scope.userLocation.address = usercity;
+        } else {
+            $scope.userLocation.address = "";
+        }
+        if (userstate) {
+            if (usercity) {
+                $scope.userLocation.address += ", ";
+            }
+            $scope.userLocation.address += userstate;
+        }
+	}
+
+    function tick() {
         $scope.clock = Date.now() // get the current time
         $timeout(tick, $scope.tickInterval); // reset the timer
     }
